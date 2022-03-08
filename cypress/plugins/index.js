845bbb0251c08e9b5f6b235b299137c5f3b88a19
Walmyr <wlsf82@gmail.com>
2022-03-08 17:33:50 +0100

    Refactor the whole thing

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,22 +1,31 @@
-/// <reference types="cypress" />
-// ***********************************************************
-// This example plugins/index.js can be used to load plugins
-//
-// You can change the location of this file or turn off loading
-// the plugins file with the 'pluginsFile' configuration option.
-//
-// You can read more here:
-// https://on.cypress.io/plugins-guide
-// ***********************************************************
-
-// This function is called when a project is opened or re-opened (e.g. due to
-// the project's config changing)
-
 /**
  * @type {Cypress.PluginConfig}
  */
-// eslint-disable-next-line no-unused-vars
+
+const { generate } = require('gerador-validador-cpf')
+
 module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
+  on('task', {
+    consumerFactory() {
+      let firstName = 'Walmyr'
+      let lastName = 'Filho'
+
+      return {
+        name: `${firstName} ${lastName}`,
+        cpf: generate(),
+        email: `${firstName.toLowerCase()}${lastName.toLowerCase()}@example.com`,
+        whatsapp: '19999999999',
+        address: {
+          postalcode: '13476562',
+          street: 'Rua Irmã Dorothy Mae Stang',
+          number: '1000',
+          details: 'Teste de software',
+          district: 'Loteamento Residencial de Interesse Social Vila Nova Esperança - ASTA 4',
+          city_state: 'Americana/SP'
+        }
+      }
+    }
+  })
+
+  return config
 }
