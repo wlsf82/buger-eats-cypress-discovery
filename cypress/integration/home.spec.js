e0a91396d825a14d3b8c7b6fccc4ca26179d7d7b
Walmyr <wlsf82@gmail.com>
2022-03-08 17:33:50 +0100

    Refactor the whole thing

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,7 +1,11 @@
 /// <reference types="cypress" />
-describe('home page', () => {
-    it('App deve estar online', () => {
-        cy.visit('/')
-        cy.get('#page-home main h1').should("have.text", "Seja um parceiro entregador pela Buger Eats")
-    })
+
+describe('Homepage', () => {
+  it('is alive', () => {
+    cy.visit('/')
+
+    cy.get('#page-home main h1')
+      .should('have.text', 'Seja um parceiro entregador pela Buger Eats')
+      .and('be.visible')
+  })
 })
